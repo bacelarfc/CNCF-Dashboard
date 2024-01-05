4d413bd2482397efdbccc2553deaae15279e79fd
bacelarfc <fernandacbacelar@gmail.com>
2024-01-05 02:41:38 +0100

    Adjustments and refactoring

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -3,7 +3,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   preprocess: vitePreprocess({
     scss: {
-      // you can add global SCSS files to be accessible in every component
       prependData: `
       @import 'src/styles/dashboard.scss';
       @import 'src/styles/logintest.scss'; 
@@ -11,7 +10,5 @@ export default {
       @import 'src/styles/userDetails.scss'; 
     `
     },
-    // ...other preprocessors like PostCSS, TypeScript, etc.
   }),
-  // ...other configurations
 };
